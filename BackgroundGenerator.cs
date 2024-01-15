549b5ecfa845e8ca33d21673a21ae739dc0b5958
NikolaMilosa <nikolamilosa20@gmail.com>
2024-01-15 09:04:56 +0100

    BackgroundWorker example

---
+++ BackgroundGenerator.cs
@@ -0,0 +1,54 @@
+
+
+
+using System.Threading.Channels;
+
+class BackgroundGenerator<T> : IHostedService
+{
+    private readonly ILogger<BackgroundGenerator<T>> _logger;
+    private readonly ChannelReader<T> _requests;
+    private readonly int _maxBatchSize;
+
+    public BackgroundGenerator(ILogger<BackgroundGenerator<T>> logger, ChannelReader<T> requests, int maxBatchSize)
+    {
+        _logger = logger;
+        _requests = requests;
+        _maxBatchSize = maxBatchSize;
+    }
+
+    public async Task StartAsync(CancellationToken cancellationToken)
+    {
+        Task.Run(async () => await RunThread(cancellationToken));
+    }
+
+    public async Task RunThread(CancellationToken cancellationToken) {
+        // Normal work, until the API is running
+        _logger.LogInformation("Starting execution of background service...");
+
+        while(await _requests.WaitToReadAsync(cancellationToken)) {
+            _logger.LogInformation("Received data, creating a batch...");
+            var batch = new List<T>();
+            while(_requests.TryRead(out T item)){
+                batch.Add(item);
+                if (batch.Count == _maxBatchSize) break;
+            }
+            await ExecuteLogic(batch);
+        }
+    } 
+
+    public Task StopAsync(CancellationToken cancellationToken)
+    {
+        _logger.LogInformation("Received stop signal, waiting for all batches to process...");
+        // Either save the status of Queue in some file so when the service restarts if the work is important
+        // Or just exit
+        _logger.LogInformation("Stopping generator...");
+        return Task.CompletedTask;
+    }
+
+    private async Task ExecuteLogic(List<T> batch) {
+        foreach(var item in batch) {
+            _logger.LogInformation($"Processing item [{item}]");
+            await Task.Delay(1000);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,64 @@
+using System.Threading.Channels;
+using Microsoft.AspNetCore.Mvc;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var channel = Channel.CreateUnbounded<Guid>(new UnboundedChannelOptions {
+    SingleReader = true,
+    SingleWriter = false,
+});
+builder.Services.AddSingleton(channel);
+builder.Services.AddHostedService(x => new BackgroundGenerator<Guid>(x.GetRequiredService<ILogger<BackgroundGenerator<Guid>>>(), channel.Reader, 5));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+// app.UseHttpsRedirection();
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/weatherforecast", () =>
+{
+    var forecast =  Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+    return forecast;
+})
+.WithName("GetWeatherForecast")
+.WithOpenApi();
+
+app.MapGet("/process", async ([FromServices] Channel<Guid> channel, uint num) => {
+    for (int i = 0; i < num; i++) {
+        await channel.Writer.WriteAsync(Guid.NewGuid());
+    }
+
+    return "Ok";
+})
+.WithName("Process")
+.WithOpenApi();
+
+app.Run();
+
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
